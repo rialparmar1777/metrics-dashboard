ad2401165473328f0756cbadf040820066f7021f
Rial Parmar <rialparmar007@gmail.com>
2025-03-10 22:59:12 -0400

    Solved packages errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    darkMode: 'class', // Enable dark mode
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Enable dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
